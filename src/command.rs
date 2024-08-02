ea089ab52e5489704b38258a2dffd69cfc19976a
苏向夜 <fu050409@163.com>
2024-08-02 11:20:25 +0800

    docs: improve doc string of `run` function

--- src/command.rs
+++ src/command.rs
@@ -10,6 +10,17 @@ pub fn define_command(options: Command) -> Command {
   options
 }
 
+/// Run command
+///
+/// **NOTE**: If the given `args` is empty, it will use `process.argv` instead.
+///
+/// **NOTE**: The given `args` should include the nodejs executable and script name.
+/// For example, if you are running a script `index.js` in the current directory with
+/// a flag `--foo`, you should pass `["node", "index.js", "--foo"]` as `args`.
+///
+/// @param cmd Command object
+/// @param args Run with given arguments
+/// @returns {void}
 #[napi]
 pub fn run(env: Env, cmd: Command, args: Option<Vec<String>>) -> Result<()> {
   let raw_args = resolve_option_args(env, args)?;
