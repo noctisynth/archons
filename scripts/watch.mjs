f4f123123dd943e8b70f9ead2016417e507e6fd3
苏向夜 <fu050409@163.com>
2025-01-26 11:03:55 +0800

    chore(script): optimize watch script

--- scripts/watch.mjs
+++ scripts/watch.mjs
@@ -4,6 +4,7 @@ import { fileURLToPath } from 'node:url'
 import path from 'node:path'
 import fs from 'node:fs'
 import { createSpinner } from '../index.js'
+import chalk from 'chalk'
 
 /**
  * @param {string} filename
@@ -26,26 +27,32 @@ function tryBuild(cmd, msg, spinner) {
   try {
     spinner.setMessage(`${msg}`)
     execSync(cmd, { stdio: 'inherit' })
-    console.clear()
   } catch (e) {
     console.log(e)
     spinner.println('[Archons] Build failed.')
   }
 }
 
+function createArchonsSpinner() {
+  const spinner = createSpinner()
+  spinner.setTemplate('{prefix:.green} {spinner:.yellow} {msg}')
+  spinner.setPrefix('[Archons]')
+  return spinner
+}
+
 const dirPath = path.join(path.dirname(fileURLToPath(import.meta.url)), '..')
 const ignored = ['node_modules', '.git', '.github', '.vscode', 'dist', 'build', 'bin', '.md', '.d.ts', 'target', '.cjs']
+const greenPrefix = chalk.green('[Archons]')
 
-const spinner = createSpinner();
-spinner.setTemplate('{prefix} {spinner} {msg}')
+const spinner = createArchonsSpinner();
 spinner.enableSteadyTick(100)
-spinner.setPrefix('[Archons]')
 spinner.setMessage('Initial building...')
 tryBuild('yarn build:debug', 'Building module...', spinner)
 tryBuild('yarn build:examples', 'Building examples...', spinner)
 spinner.finishAndClear()
-console.log('[Archons] Build complete.\n')
-console.log(`[Archons] Watching on ${dirPath} for changes...`)
+console.clear()
+console.log(`${greenPrefix} Build complete.\n`)
+console.log(`${greenPrefix} Watching on ${chalk.cyan(dirPath)} for changes...`)
 
 let lastBuild = Date.now()
 fs.watch(dirPath, { recursive: true }, (eventType, filename) => {
@@ -54,18 +61,16 @@ fs.watch(dirPath, { recursive: true }, (eventType, filename) => {
     return
   }
   if (filename && !isIgnored(filename)) {
-    const spinner = createSpinner()
-    spinner.setTemplate('{prefix} {spinner} {msg}')
-    spinner.setPrefix('[Archons]')
-    spinner.println(`[Archons] File ${filename} was ${eventType}d, rebuilding...`)
-    spinner.setMessage('Rebuilding...')
+    const spinner = createArchonsSpinner()
+    spinner.println(`${greenPrefix} File ${chalk.cyan(filename)} was ${eventType}d, rebuilding...`)
     spinner.enableSteadyTick(100)
     if (filename.endsWith('.rs') || filename.endsWith('.toml')) {
       tryBuild('yarn build:debug', 'Rebuilding module...', spinner)
     }
     tryBuild('yarn build:examples', 'Rebuilding examples...', spinner)
-    spinner.println('[Archons] Build complete.\n\n')
-    spinner.println('[Archons] Watching for changes...')
+    console.clear()
+    spinner.println(`${greenPrefix} Build complete.\n\n`)
+    spinner.println(`${greenPrefix} Watching for changes...`)
     spinner.finishAndClear()
     lastBuild = Date.now()
   }
